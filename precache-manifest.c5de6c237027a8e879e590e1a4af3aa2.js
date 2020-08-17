0afc23a13fc3f3aed4adb3a539419fe51316046a
jakubj <jakubjuraszek@yahoo.com>
2020-08-17 07:47:32 +0200

    Updates

---
+++ precache-manifest.c5de6c237027a8e879e590e1a4af3aa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d168fb3e6faa7c673b5eb665b1737c88",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "16a5be3eb41be84e606e",
+    "url": "/weather-app/static/css/main.f9fe2bc9.chunk.css"
+  },
+  {
+    "revision": "3936a0b0eba44d9b334e",
+    "url": "/weather-app/static/js/2.662bb4dc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weather-app/static/js/2.662bb4dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a5be3eb41be84e606e",
+    "url": "/weather-app/static/js/main.490b8184.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "1828d026a93f77a1f9554789155c0aee",
+    "url": "/weather-app/static/media/background-image.1828d026.png"
+  }
+]);
