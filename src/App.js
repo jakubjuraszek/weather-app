49643c48c0e3a334c4292b9331182d9a8f212dbc
jakubj <jakubjuraszek@yahoo.com>
2020-08-17 20:35:14 +0200

    moving Home to components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import Home from './common/NotFound/Home/Home';
+import Home from './components/Home/Home';
 import SearchInputContainer from './containers/SearchInputContainer';
 import CardContainer from './containers/CardContainer';
 import HeaderContainer from './containers/HeaderContainer';
